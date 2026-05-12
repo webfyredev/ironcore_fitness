7609faabb8148b14350c51cf3dc1164153b80889
impphilwebs <imphilwebs@gmail.com>
2026-05-12 22:18:47 +0100

    Quick changes to the map on the contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -116,7 +116,7 @@ export default function Contacts(){
                             width="100%" 
                             height="100%" 
                             style={{border:0}}
-                            allowfullscreen="" 
+                            allowFullScreen = ""
                             loading="lazy" 
                             referrerpolicy="no-referrer-when-downgrade">
                         </motion.iframe>
